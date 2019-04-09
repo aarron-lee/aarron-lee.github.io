2f62bbb38be7d9a1a7c80d0e31ba62d6fe8e16c6
Aarron Lee <aclee91@gmail.com>
2019-04-09 09:01:48 -0400

    add theme colors to card

--- src/components/presentation/card/card.jsx
+++ src/components/presentation/card/card.jsx
@@ -1,7 +1,14 @@
 import React from "react"
 
 import styles from "./cardStyles.module.scss"
+import ThemeConsumer from "../../util/theme/themeConsumer"
 
-const Card = ({ children }) => <div className={styles.card}>{children}</div>
+const Card = ({ children }) => (
+  <ThemeConsumer>
+    {({ themeStyles: { cardStyles } }) => (
+      <div className={`${styles.card} ${cardStyles}`}>{children}</div>
+    )}
+  </ThemeConsumer>
+)
 
 export default Card
