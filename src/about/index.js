e14e5c1ec2fc32fff7672833a52eb98a5ff5c471
Aarron Lee <aclee91@gmail.com>
2019-04-09 18:30:45 -0400

    add favicon, card to about page

--- src/about/index.js
+++ src/about/index.js
@@ -3,9 +3,16 @@ import React from "react"
 import "../styles/prism-atom-dark.css"
 
 import Page from "../templates/page"
+import Card from "../components/presentation/card/card"
 
 const Layout = () => {
-  return <Page>about me</Page>
+  return (
+    <Page>
+      <Card>
+        <h2>About Me</h2>
+      </Card>
+    </Page>
+  )
 }
 
 export default Layout
